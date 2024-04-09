9c21a805bd4569265c1978d0da975a8de504f444
Moasfar Javed <88271241+Moasfar-Javed@users.noreply.github.com>
2024-04-09 12:04:32 +0500

    .

--- api/index.mjs
+++ api/index.mjs
@@ -1,23 +1,23 @@
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import userRoutes from "./src/routes/user_routes.mjs";
-import passRoutes from "./src/routes/pass_routes.mjs";
-import categoryRoutes from "./src/routes/category_routes.mjs";
-import eventRoutes from "./src/routes/event_routes.mjs";
-import statsRoutes from "./src/routes/stats_route.mjs";
-import utilityRoutes from "./src/routes/utility_routes.mjs";
+import userRoutes from "../src/routes/user_routes.mjs";
+import passRoutes from "../src/routes/pass_routes.mjs";
+import categoryRoutes from "../src/routes/category_routes.mjs";
+import eventRoutes from "../src/routes/event_routes.mjs";
+import statsRoutes from "../src/routes/stats_route.mjs";
+import utilityRoutes from "../src/routes/utility_routes.mjs";
 // import app from "./server.mjs";
 import { MongoClient, ServerApiVersion } from "mongodb";
-import appConfig from "./src/config/app_config.mjs";
-import databaseConfig from "./src/config/database_config.mjs";
-import UserService from "./src/services/user_service.mjs";
-import PassService from "./src/services/pass_service.mjs";
-import CategoryService from "./src/services/category_service.mjs";
-import EventService from "./src/services/event_service.mjs";
-import EmailUtility from "./src/utility/email_util.mjs";
-import FirebaseUtility from "./src/utility/fcm_utility.mjs";
-import UtilityService from "./src/services/utility_service.mjs";
+import appConfig from "../src/config/app_config.mjs";
+import databaseConfig from "../src/config/database_config.mjs";
+import UserService from "../src/services/user_service.mjs";
+import PassService from "../src/services/pass_service.mjs";
+import CategoryService from "../src/services/category_service.mjs";
+import EventService from "../src/services/event_service.mjs";
+import EmailUtility from "../src/utility/email_util.mjs";
+import FirebaseUtility from "../src/utility/fcm_utility.mjs";
+import UtilityService from "../src/services/utility_service.mjs";
 
 const port = appConfig.server.port;
 const smtpConfig = {
@@ -72,7 +72,7 @@ client
     await EventService.connectDatabase(client);
     await UtilityService.connectDatabase(client);
     EmailUtility.initialize(smtpConfig);
-    FirebaseUtility.initializeApp();
+    // FirebaseUtility.initializeApp();
     app.listen(port, () => {
       console.log(`http server running => ${port}`);
     });
