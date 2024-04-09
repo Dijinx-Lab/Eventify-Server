fd3c58e4c283b42c14fc38d12a6ea1cfe6cc6176
Moasfar Javed <88271241+Moasfar-Javed@users.noreply.github.com>
2024-04-09 12:12:43 +0500

    added server.mjs

--- index.mjs
+++ index.mjs
@@ -1,23 +1,14 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
-import userRoutes from "./src/routes/user_routes.mjs";
-import passRoutes from "./src/routes/pass_routes.mjs";
-import categoryRoutes from "./src/routes/category_routes.mjs";
-import eventRoutes from "./src/routes/event_routes.mjs";
-import statsRoutes from "./src/routes/stats_route.mjs";
-import utilityRoutes from "./src/routes/utility_routes.mjs";
-// import app from "./server.mjs";
+import app from "./server.mjs";
 import { MongoClient, ServerApiVersion } from "mongodb";
-import appConfig from "./src/config/app_config.mjs";
-import databaseConfig from "./src/config/database_config.mjs";
-import UserService from "./src/services/user_service.mjs";
-import PassService from "./src/services/pass_service.mjs";
-import CategoryService from "./src/services/category_service.mjs";
-import EventService from "./src/services/event_service.mjs";
-import EmailUtility from "./src/utility/email_util.mjs";
-import FirebaseUtility from "./src/utility/fcm_utility.mjs";
-import UtilityService from "./src/services/utility_service.mjs";
+import appConfig from "../src/config/app_config.mjs";
+import databaseConfig from "../src/config/database_config.mjs";
+import UserService from "../src/services/user_service.mjs";
+import PassService from "../src/services/pass_service.mjs";
+import CategoryService from "../src/services/category_service.mjs";
+import EventService from "../src/services/event_service.mjs";
+import EmailUtility from "../src/utility/email_util.mjs";
+import FirebaseUtility from "../src/utility/fcm_utility.mjs";
+import UtilityService from "../src/services/utility_service.mjs";
 
 const port = appConfig.server.port;
 const smtpConfig = {
@@ -40,25 +31,6 @@ const client = new MongoClient(uri, {
   },
 });
 
-const app = express();
-
-app.use(cors());
-app.use(bodyParser.json());
-
-const baseUrl = "/api/v1/eventify";
-
-app.use(baseUrl, userRoutes);
-app.use(baseUrl, passRoutes);
-app.use(baseUrl, categoryRoutes);
-app.use(baseUrl, eventRoutes);
-app.use(baseUrl, statsRoutes);
-app.use(baseUrl, utilityRoutes);
-
-app.use("/", (req, res) =>
-  res.status(200).json({
-    message: "Hello",
-  })
-);
 client
   .connect()
   .catch((err) => {
