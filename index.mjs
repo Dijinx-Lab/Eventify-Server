6c8e5146baada6b223fad9d0ffe065b1f2b814a4
Moasfar Javed <88271241+Moasfar-Javed@users.noreply.github.com>
2024-10-14 16:19:13 +0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -31,8 +31,8 @@ const smtpConfig = {
 
 const username = encodeURIComponent(databaseConfig.database.username);
 const password = encodeURIComponent(databaseConfig.database.password);
-// const uri = `mongodb+srv://${username}:${password}@eventbazaar.y8gsrgm.mongodb.net/?retryWrites=true&w=majority&appName=${databaseConfig.database.dbName}`;
-const uri = `mongodb://${databaseConfig.database.host}:${databaseConfig.database.port}/${databaseConfig.database.dbName}`;
+const uri = `mongodb+srv://${username}:${password}@eventbazaar.y8gsrgm.mongodb.net/?retryWrites=true&w=majority&appName=${databaseConfig.database.dbName}`;
+// const uri = `mongodb://${databaseConfig.database.host}:${databaseConfig.database.port}/${databaseConfig.database.dbName}`;
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
